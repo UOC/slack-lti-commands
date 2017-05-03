f252315f8bb9b603df884a1cc95d99bb935797ac
Xavi Aracil <xavi@laibeth.com>
2017-05-03 15:52:44 +0200

    Basic project from https://start.spring.io, with Web + JPA

---
+++ src/main/java/edu/uoc/elc/slack/lti/SlackLtiCommandApplication.java
@@ -0,0 +1,12 @@
+package edu.uoc.elc.slack.lti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackLtiCommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackLtiCommandApplication.class, args);
+	}
+}
